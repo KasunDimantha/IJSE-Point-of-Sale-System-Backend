6f791ec380690d5f4b1269fd36e8b060d5072362
KasunDimantha <kasundimantha97@gmail.com>
2024-05-27 08:14:59 +0530

    create order service

--- POS/src/main/java/com/example/POS/service/OrderService.java
+++ POS/src/main/java/com/example/POS/service/OrderService.java
@@ -1,5 +1,16 @@
 package com.example.POS.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.POS.entity.Order;
+
+@Service
 public interface OrderService {
     
+    List<Order> getAllOrders();
+    Order getOrderById(Long id);
+    Order createOrder(Order order);
+    void deleteOrder(Long id);
 }
