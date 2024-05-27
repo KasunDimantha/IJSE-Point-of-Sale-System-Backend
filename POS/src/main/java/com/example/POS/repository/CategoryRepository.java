7536ae1576e67acda8b908ab4e1c93e2038b1b7e
KasunDimantha <kasundimantha97@gmail.com>
2024-05-27 07:52:29 +0530

    create Orderitem Service

--- POS/src/main/java/com/example/POS/repository/CategoryRepository.java
+++ POS/src/main/java/com/example/POS/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.example.POS.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.POS.entity.Category;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
     
 }
--- POS/src/main/java/com/example/POS/service/OrderitemService.java
+++ POS/src/main/java/com/example/POS/service/OrderitemService.java
@@ -1,9 +1,15 @@
 package com.example.POS.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.POS.entity.OrderItem;
 
 @Service
 public interface OrderitemService {
     
-
+    List<OrderItem> getAllProductByOrderId(Long id);
+    OrderItem addOrderItemToOrder(Long orderId, Long productId, Double quantity);
+    void deleteOrderItemFromOrder(Long orderId, Long productId);
 }
