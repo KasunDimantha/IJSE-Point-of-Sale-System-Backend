002c43dfc3b576816ef3f8301013502967eb1b3b
KasunDimantha <kasundimantha97@gmail.com>
2024-05-27 09:07:05 +0530

    update order controller

--- POS/src/main/java/com/example/POS/controller/OrderController.java
+++ POS/src/main/java/com/example/POS/controller/OrderController.java
@@ -1,15 +1,20 @@
 package com.example.POS.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.POS.dto.OrderDto;
 import com.example.POS.entity.Order;
 import com.example.POS.service.OrderService;
 
@@ -42,5 +47,40 @@ public class OrderController {
         }
     }
 
-    
+    @PostMapping("/orders")
+    public ResponseEntity<Order> createOrder() {
+        Order order = new Order();
+
+        order.setTotalPrice(0.0);
+        order.setOrderDate(LocalDateTime.now());
+
+        Order newOrder = orderService.createOrder(order);
+
+        if(newOrder != null) {
+            return ResponseEntity.status(201).body(newOrder);
+        } else {
+            return ResponseEntity.status(400).build();
+        }
+    }
+
+    @PutMapping("/orders/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable Long id, @RequestBody OrderDto orderDto) {
+        
+        Order order = new Order();
+
+        order.setTotalPrice(orderDto.getTotalPrice());
+
+        Order updatedOrder = orderService.updateOrder(id, order);
+
+        if(updatedOrder != null) {
+            return ResponseEntity.status(200).body(updatedOrder);
+        } else {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    @DeleteMapping("/orders/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+    }
 }
