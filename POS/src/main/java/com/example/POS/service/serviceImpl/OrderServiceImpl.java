fa3bde041974580da5846be109c75381f199c052
KasunDimantha <kasundimantha97@gmail.com>
2024-05-27 08:21:51 +0530

    create OrderService Impl

---
+++ POS/src/main/java/com/example/POS/service/serviceImpl/OrderServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.POS.service.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.POS.entity.Order;
+import com.example.POS.repository.OrderRepository;
+import com.example.POS.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public void deleteOrder(Long id) {
+        orderRepository.deleteById(id);
+    }
+}
