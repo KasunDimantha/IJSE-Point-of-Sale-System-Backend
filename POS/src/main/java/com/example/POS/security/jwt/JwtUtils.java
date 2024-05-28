3c1f68609db408ec2db1ac9181bddf24228b0b8a
KasunDimantha <kasundimantha97@gmail.com>
2024-05-28 10:04:55 +0530

    generate jwt token

--- POS/src/main/java/com/example/POS/security/jwt/JwtUtils.java
+++ POS/src/main/java/com/example/POS/security/jwt/JwtUtils.java
@@ -1,10 +1,15 @@
 package com.example.POS.security.jwt;
 
 import java.security.Key;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -17,5 +22,19 @@ public class JwtUtils {
 
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+
+    
+    public String generateJwtToken(Authentication authentication) {
+
+        UserDetails UserDetails = (UserDetails) authentication.getPrincipal();
+        // generate jwt token for the userdetails
+        return Jwts.builder()
+                .setSubject(UserDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 1000 * 60 * 60 * 24))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+
     }
 }
