ef9d8311d4ba613745903026f1f1d87cc3b7b714
KasunDimantha <kasundimantha97@gmail.com>
2024-05-29 13:56:19 +0530

    create login dto

--- POS/src/main/java/com/example/POS/controller/AuthController.java
+++ POS/src/main/java/com/example/POS/controller/AuthController.java
@@ -1,12 +1,13 @@
 package com.example.POS.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+
+@RestController
+@CrossOrigin(origins = "*")
 public class AuthController {
     
     @PostMapping("/auth/login")
